cc71698e694312887e77e1584b45bb6b9a9fadfa
fortunatoRoncholeta <fortunatoroncholeta@gmail.com>
2022-10-12 23:17:27 -0300

    exercicios 1000,1001,1002

---
+++ 1001.py
@@ -0,0 +1,7 @@
+#indentificando que as entradas serao inteiras , para nao confundir a linguagem
+A = int(input())
+B = int(input())
+
+X = A + B
+
+print('X = {}'.format(X))
---
+++ 1002.py
@@ -0,0 +1,8 @@
+#convertendo pra float
+raio= float(input())
+
+n=3.14159
+#** elevado ao quadrado
+area = n*(raio**2)
+#{:.4f} formatar com casas decimais
+print('A={:.4f}'.format(area))
